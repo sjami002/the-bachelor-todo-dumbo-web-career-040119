8299560a9ca73ecb5cbd6fafe2762cb3df804ca6
Shoptorshi Jamil <sjami002@gmail.com>
2019-03-20 01:34:12 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,5 +1,13 @@
 def get_first_name_of_season_winner(data, season)
   # code here
+  data[season].each do |bio|
+    bio.each do |key, value|
+      if value == "Winner"
+        fullname = bio["name"]
+        return fullname.split(" ").first
+      end
+    end
+  end
 end
 
 def get_contestant_name(data, occupation)
